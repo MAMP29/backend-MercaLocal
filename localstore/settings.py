2c772395f25c1acbcea95577cd8c1cd384d563b3
anderson_pc <anderson19pc98@gmail.com>
2024-11-05 21:36:02 -0500

    (pip install django-cors-headers) permisos cors para la implementacion local.

--- localstore/settings.py
+++ localstore/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'authentication',
     'dashboard',
     'users',
+     'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +59,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     #'allauth.account.middleware.AccountMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    #"https://tu-dominio-2.com",
 ]
 
 ROOT_URLCONF = 'localstore.urls'
