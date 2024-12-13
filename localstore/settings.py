4dc057f584bb7b9f2783cb615d246897b243383a
Miguel <106500061+MAMP29@users.noreply.github.com.>
2024-12-13 11:15:14 -0500

    Actualización de host permitidos

--- localstore/settings.py
+++ localstore/settings.py
@@ -23,13 +23,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-qu@y+3tj5=4rygyjvv9+!kz*vc5!vjh7mxd16m&g_gm=3bwf4g')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-qu@y+3tj5=4rygyjvv9+!kz*vc5!vjh7mxd16m&g_gm=3bwf4g')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-#DEBUG = 'RENDER' not in os.environ
+#DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(' ') if not DEBUG else []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='', cast=str).split(' ') if not DEBUG else []
+
 
 #ALLOWED_HOSTS = []
 # Application definition
